a43c5a0c4939e626853651142096adbfd8b98524
ryokosuge <ryo.kosuge@gmail.com>
2019-12-16 15:24:00 +0900

    feat:	Create BannerActivity

---
+++ app/src/main/java/jp/fluct/fluctsdk/exmaple/android/gmamediation/BannerActivity.kt
@@ -0,0 +1,12 @@
+package jp.fluct.fluctsdk.exmaple.android.gmamediation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class BannerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.banner_activity)
+    }
+}
